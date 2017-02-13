95f66a669bd8292d62be724ec7df73be0487fe91
Catalin Tudorache <tudorache.catalin47@gmail.com>
2017-02-13 22:16:52 +0200

    WIP d3 heat map

--- src/js/components/TripsSunburst.js
+++ src/js/components/TripsSunburst.js
@@ -1,10 +1,36 @@
 import React from "react";
+import moment from "moment";
+
+import heatMap from "../heatMap";
 
 export default class TripsSunburst extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    // console.log(this.props.trips);
+    this.data = [];
+    this.props.trips.forEach((item) => {
+      var day = moment.unix(item.start_time).day() + 1;
+      var hour = moment.unix(item.start_time).hour();
+      var found = false;
+      this.data.forEach((dataItem) => {
+        if(dataItem.day === day && dataItem.hour === hour) {
+          dataItem.value++;
+          found = true;
+        }
+      });
+      if(found == false) {
+        this.data.push({day: day, hour: hour, value: 1});
+      }
+    });
+    heatMap(this.data);
+  }
+
   render() {
     return (
-      <div>
-        {this.props.trips.length}
+      <div id="chart">
       </div>
     );
   }
